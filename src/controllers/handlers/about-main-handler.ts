42c5c57ebafa199ab244a683c8dfa4f5fcaef330
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-10-29 14:31:58 -0300

    feat: remove EU1 Peer from blacklist

--- src/controllers/handlers/about-main-handler.ts
+++ src/controllers/handlers/about-main-handler.ts
@@ -2,7 +2,7 @@ import { HandlerContextWithPath, ServiceUnavailableError } from '../../types'
 import { About } from '@dcl/catalyst-api-specs/lib/client'
 import { randomInt } from 'crypto'
 
-const BLACKLISTED_CATALYSTS = ['peer-eu1.decentraland.org']
+const BLACKLISTED_CATALYSTS: [] = []
 
 export async function aboutMainHandler(
   context: Pick<HandlerContextWithPath<'catalystsProvider' | 'mainRealmProvider', '/main/about'>, 'components' | 'url'>
@@ -16,16 +16,18 @@ export async function aboutMainHandler(
     throw new ServiceUnavailableError('No content catalysts available')
   }
 
-  const filteredCatalysts = catalysts.filter(
-    (catalyst: any) =>
-      !BLACKLISTED_CATALYSTS.some((blackListedCatalyst) => catalyst.url.toLowerCase().includes(blackListedCatalyst))
-  )
+  const filteredCatalysts = BLACKLISTED_CATALYSTS.length
+    ? catalysts.filter(
+        (catalyst: any) =>
+          !BLACKLISTED_CATALYSTS.some((blackListedCatalyst) => catalyst.url.toLowerCase().includes(blackListedCatalyst))
+      )
+    : catalysts
 
   let index = randomInt(filteredCatalysts.length)
 
   const preferredCatalyst = context.url.searchParams.get('catalyst')
   if (preferredCatalyst) {
-    const preferredCatalystIndex = filteredCatalysts.findIndex((catalyst) => catalyst.url === preferredCatalyst)
+    const preferredCatalystIndex = filteredCatalysts.findIndex((catalyst: any) => catalyst.url === preferredCatalyst)
     if (preferredCatalystIndex >= 0) {
       index = preferredCatalystIndex
     }
