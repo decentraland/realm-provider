47cea136a17aae27f2fd328e955b42887c73571f
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2024-11-05 12:04:12 -0300

    feat: remove EU1 Peer from blacklist (#61)
    * feat: remove EU1 Peer from blacklist
    
    * test: about handler

--- src/controllers/handlers/about-main-handler.ts
+++ src/controllers/handlers/about-main-handler.ts
@@ -2,30 +2,34 @@ import { HandlerContextWithPath, ServiceUnavailableError } from '../../types'
 import { About } from '@dcl/catalyst-api-specs/lib/client'
 import { randomInt } from 'crypto'
 
-const BLACKLISTED_CATALYSTS = ['peer-eu1.decentraland.org']
-
 export async function aboutMainHandler(
-  context: Pick<HandlerContextWithPath<'catalystsProvider' | 'mainRealmProvider', '/main/about'>, 'components' | 'url'>
+  context: Pick<
+    HandlerContextWithPath<'catalystsProvider' | 'mainRealmProvider' | 'config', '/main/about'>,
+    'components' | 'url'
+  >
 ): Promise<{ status: 200; body: About }> {
   const {
-    components: { catalystsProvider, mainRealmProvider }
+    components: { catalystsProvider, mainRealmProvider, config }
   } = context
+  const blacklistedCatalyst = ((await config.getString('BLACKLISTED_CATALYST')) || '').split(';').filter(Boolean)
   const catalysts = await catalystsProvider.getHealhtyCatalysts()
 
   if (catalysts.length === 0) {
     throw new ServiceUnavailableError('No content catalysts available')
   }
 
-  const filteredCatalysts = catalysts.filter(
-    (catalyst: any) =>
-      !BLACKLISTED_CATALYSTS.some((blackListedCatalyst) => catalyst.url.toLowerCase().includes(blackListedCatalyst))
-  )
+  const filteredCatalysts = blacklistedCatalyst.length
+    ? catalysts.filter(
+        (catalyst: any) =>
+          !blacklistedCatalyst.some((blackListedCatalyst) => catalyst.url.toLowerCase().includes(blackListedCatalyst))
+      )
+    : catalysts
 
   let index = randomInt(filteredCatalysts.length)
 
   const preferredCatalyst = context.url.searchParams.get('catalyst')
   if (preferredCatalyst) {
-    const preferredCatalystIndex = filteredCatalysts.findIndex((catalyst) => catalyst.url === preferredCatalyst)
+    const preferredCatalystIndex = filteredCatalysts.findIndex((catalyst: any) => catalyst.url === preferredCatalyst)
     if (preferredCatalystIndex >= 0) {
       index = preferredCatalystIndex
     }
